4042d7e06148f6870747d5104a6a386625a978eb
Brokenboygirl <107098215+Brokenboygirl@users.noreply.github.com>
2022-12-25 17:46:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ API_HASH = getenv("API_HASH", "84ff6bdb8eeb6e8bedf8a8192e3da3dd") #optional
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5220826180").split()))
 OWNER_ID = int(getenv("OWNER_ID", "5220826180"))
 MONGO_URL = getenv("MONGO_URL","mongodb+srv://AbishnoiMusic:AbishnoiMusic29@cluster0.k7sfn.mongodb.net/?retryWrites=true&w=majority")
-BOT_TOKEN = getenv("BOT_TOKEN", "5735720651:AAGvDexNZpcGphjn7Cxyy2bzi4y541K-NjI")
+BOT_TOKEN = getenv("BOT_TOKEN", "5885664286:AAF1J6E9h0-nBovOaV1JF8zI1thz6n4u93M")
 ALIVE_PIC = getenv("ALIVE_PIC","https://te.legra.ph/file/cec5a117ff959166f7b6f.jpg")
 ALIVE_TEXT = getenv("ALIVE_TEXT","Nehal")
 PM_LOGGER = getenv("PM_LOGGER","-1001598879298")
