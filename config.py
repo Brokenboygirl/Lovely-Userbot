926443c9b0988bc21584f107fa50fb3b6a54dd06
Brokenboygirl <107098215+Brokenboygirl@users.noreply.github.com>
2023-01-04 17:01:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ OWNER_ID = int(getenv("OWNER_ID", "5440768701"))
 MONGO_URL = getenv("MONGO_URL","mongodb+srv://AbishnoiMusic:AbishnoiMusic29@cluster0.k7sfn.mongodb.net/?retryWrites=true&w=majority")
 BOT_TOKEN = getenv("BOT_TOKEN", "5802682449:AAF5w7g5bAUVRqrgGX9Bni5wpUiTe9yuYw0")
 ALIVE_PIC = getenv("ALIVE_PIC","https://te.legra.ph/file/cec5a117ff959166f7b6f.jpg")
-ALIVE_TEXT = getenv("ALIVE_TEXT","Nehal")
+ALIVE_TEXT = getenv("ALIVE_TEXT","NehalG")
 PM_LOGGER = getenv("PM_LOGGER","")
 LOG_GROUP = getenv("LOG_GROUP","-1001598879298")
 
