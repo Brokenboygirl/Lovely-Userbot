b515eaba0612ec1eb260ed1eb232de13fe4f65be
Brokenboygirl <107098215+Brokenboygirl@users.noreply.github.com>
2023-01-31 20:31:11 +0530

    Update afk.py

--- Zaid/modules/basic/afk.py
+++ Zaid/modules/basic/afk.py
@@ -5,8 +5,8 @@ import humanize
 from pyrogram import filters, Client
 from pyrogram.types import Message
 
-from Zaid.helper.PyroHelpers import GetChatID, ReplyCheck
-from Zaid.modules.help import add_command_help
+from Lovely.helper.PyroHelpers import GetChatID, ReplyCheck
+from Lovely.modules.help import add_command_help
 
 AFK = False
 AFK_REASON = ""
