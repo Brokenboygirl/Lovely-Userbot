960ebdafe70aa8e11ecf177846e94c6f10fa6f86
Brokenboygirl <107098215+Brokenboygirl@users.noreply.github.com>
2023-01-31 21:26:56 +0530

    Update start.py

--- Zaid/modules/bot/start.py
+++ Zaid/modules/bot/start.py
@@ -1,7 +1,7 @@
-from Zaid import app
+from Lovely import app
 from pyrogram import filters
 
 
 @app.on_message(filters.command("start") & filters.private)
 async def start(client, message):
-   await message.reply_text("Hey Zaid Userbot Assistant here")
+   await message.reply_text("Hey Lovely Userbot Assistant here")
