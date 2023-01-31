ae3b3b8546397a1e410975db6ac8aad87d4e7235
Brokenboygirl <107098215+Brokenboygirl@users.noreply.github.com>
2023-01-31 21:06:32 +0530

    Update sangmata.py

--- Zaid/modules/basic/sangmata.py
+++ Zaid/modules/basic/sangmata.py
@@ -6,8 +6,8 @@ from pyrogram.errors import YouBlockedUser
 from pyrogram.types import *
 
 
-from Zaid.modules.help import add_command_help
-from Zaid.modules.basic.profile import extract_user
+from Lovely.modules.help import add_command_help
+from Lovely.modules.basic.profile import extract_user
 
 @Client.on_message(filters.command(["sg", "sa", "sangmata"], ".") & filters.me)
 async def sg(client: Client, message: Message):
