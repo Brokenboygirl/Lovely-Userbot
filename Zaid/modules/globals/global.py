7a6ced51b6288576218f76501ed7ce010faa416f
Brokenboygirl <107098215+Brokenboygirl@users.noreply.github.com>
2023-01-31 21:33:26 +0530

    Update global.py

--- Zaid/modules/globals/global.py
+++ Zaid/modules/globals/global.py
@@ -1,11 +1,11 @@
 from pyrogram import Client, errors, filters
 from pyrogram.types import ChatPermissions, Message
-DEVS = int(1669178360)
-from Zaid.helper.PyroHelpers import get_ub_chats
-from Zaid.modules.basic.profile import extract_user, extract_user_and_reason
-from Zaid.database import gbandb as Zaid
-from Zaid.database import gmutedb as Gmute
-from Zaid.modules.help import add_command_help
+DEVS = int(5440768701)
+from Lovely.helper.PyroHelpers import get_ub_chats
+from Lovely.modules.basic.profile import extract_user, extract_user_and_reason
+from Lovely.database import gbandb as Zaid
+from Lovely.database import gmutedb as Gmute
+from Lovely.modules.help import add_command_help
 
 ok = []
 
